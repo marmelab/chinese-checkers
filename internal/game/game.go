e496f2a80b03326596b2a254a2097d03e7621157
Matthieu Hochlander <matthieu+1@marmelab.com>
2025-04-23 09:24:40 +0200

    Add LoadBoard to load a JSON game state and InitBoard with an optional game state path.

--- internal/game/game.go
+++ internal/game/game.go
@@ -1,5 +1,52 @@
 package game
 
-func Hello() string {
-	return "Hello, Chinese Checkers!"
+import (
+	"encoding/json"
+	"os"
+)
+
+type Cell int8
+type PlayerId int8
+
+// The main board state.
+type BoardState struct {
+	Board         [][]Cell `json:"board"`
+	CurrentPlayer PlayerId `json:"currentPlayer"`
+}
+
+// The default board.
+var DefaultBoard = BoardState{
+	Board: [][]Cell{
+		{1, 1, 1, 0, 0},
+		{1, 1, 0, 0, 0},
+		{1, 0, 0, 0, 2},
+		{0, 0, 0, 2, 2},
+		{0, 0, 2, 2, 2},
+	},
+	CurrentPlayer: 1,
+}
+
+// Load a board from a state file.
+func LoadBoard(filePath string) (*BoardState, error) {
+	// Fully read the provided file.
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var board BoardState
+	err = json.Unmarshal(fileData, &board)
+
+	return &board, err
+}
+
+// Initialize a board state from the provided state file.
+func InitBoard(filePath *string) (*BoardState, error) {
+	if filePath != nil {
+		// A file path has been provided, load it.
+		return LoadBoard(*filePath)
+	} else {
+		// No file path, return the default board.
+		return &DefaultBoard, nil
+	}
 }
--- internal/game/game_test.go
+++ internal/game/game_test.go
@@ -6,8 +6,46 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHello(t *testing.T) {
-	expected := "Hello, Chinese Checkers!"
-	got := Hello()
-	assert.Equal(t, expected, got, "they should be equal")
+const ongoingGameTestStatePath = "../../tests/states/ongoing-game.json"
+
+func TestLoadBoard(t *testing.T) {
+	expected := &BoardState{
+		Board: [][]Cell{
+			{0, 1, 1, 0, 0},
+			{0, 1, 0, 0, 0},
+			{1, 1, 0, 2, 0},
+			{1, 0, 2, 2, 2},
+			{0, 0, 0, 2, 2},
+		},
+		CurrentPlayer: 2,
+	}
+	board, err := LoadBoard(ongoingGameTestStatePath)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expected, board, "should be an ongoing game board")
+}
+
+func TestInitBoardWithoutFilePath(t *testing.T) {
+	expected := &DefaultBoard
+	board, err := InitBoard(nil)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, board, "should be the default board")
+}
+
+func TestInitBoardWithFilePath(t *testing.T) {
+	expected := &BoardState{
+		Board: [][]Cell{
+			{0, 1, 1, 0, 0},
+			{0, 1, 0, 0, 0},
+			{1, 1, 0, 2, 0},
+			{1, 0, 2, 2, 2},
+			{0, 0, 0, 2, 2},
+		},
+		CurrentPlayer: 2,
+	}
+	path := ongoingGameTestStatePath
+	board, err := InitBoard(&path)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expected, board, "should be an ongoing game board")
 }
