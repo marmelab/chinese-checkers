ced49d1598839720f18fdec0cff61678bc1cc597
Matthieu Hochlander <matthieu@marmelab.com>
2025-04-30 10:57:32 +0200

    Improve test messages.

--- web/tests/GameBoardTest.php
+++ web/tests/GameBoardTest.php
@@ -30,11 +30,11 @@ class GameBoardTest extends WebTestCase
 		$this->assertResponseIsSuccessful();
 
 		// Check game board existence and validity.
-		$this->assertSelectorExists("table#game-board.game-board", "a game board table exists");
-		$this->assertSelectorCount(10, "table#game-board td.red-target > .green.pawn", "there are 10 green pawns on the 10 red target cells");
-		$this->assertSelectorCount(10, "table#game-board td.green-target > .red.pawn", "there are 10 red pawns on the 10 green target cells");
-		$this->assertSelectorCount(7*7, "table#game-board td", "there should be 49 board cells");
-		$this->assertSelectorCount(2*7 + 1, "table#game-board th", "there should be 15 board headers (one empty)");
+		$this->assertSelectorExists("table#game-board.game-board", "should have a game board table with id and class \"game-board\"");
+		$this->assertSelectorCount(10, "table#game-board td.red-target > .green.pawn", "should have 10 green pawns on the 10 red target cells in the game board table");
+		$this->assertSelectorCount(10, "table#game-board td.green-target > .red.pawn", "should have 10 red pawns on the 10 green target cells in the game board table");
+		$this->assertSelectorCount(7*7, "table#game-board td", "there should be 49 board cells in the game board table");
+		$this->assertSelectorCount(2*7 + 1, "table#game-board th", "there should be 15 board headers (one empty) in the game board table");
 
 		// Check the 7 row headers.
 		foreach (range(0, 6) as $index)
