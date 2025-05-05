093e2cfd4c33fa56b6745b379f6a61c26a956d6b
Matthieu Hochlander <matthieu@marmelab.com>
2025-05-05 17:38:27 +0200

    Add a way to share a game and join it on another computer.

--- web/src/Controller/GameController.php
+++ web/src/Controller/GameController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\GamePlayer;
+use App\Entity\OnlinePlayer;
 use App\Exceptions\GameApiException;
 use App\Game\GameApi;
 use App\Game\GameSession;
@@ -11,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
@@ -39,7 +42,7 @@ class GameController extends AbstractController
 				return $this->redirectToRoute("localGame");
 			}
 			else
-			{ // We want to create an online game.
+			{ // We want to create an online game and register the current player as the green one.
 				$newGame = $onlineGame->newGame();
 				return $this->redirectToRoute("onlineGame", [
 					"gameId" => $newGame->getUuid(),
@@ -48,6 +51,30 @@ class GameController extends AbstractController
 		}
 
 		return $this->render("game/new.html.twig");
+	}
+
+	/**
+	 * Try to join an online game.
+	 * @param Request $request The request.
+	 * @param OnlineGame $onlineGame Online game service.
+	 * @return Response
+	 */
+	#[Route("/join", name: "joinGame")]
+	public function join(Request $request, OnlineGame $onlineGame): Response
+	{
+		// Try to find the game from its UUID.
+		if (empty($game = $onlineGame->findGame($gameId = $request->get("game"))))
+			throw $this->createNotFoundException("No game for ID $gameId");
+
+		if ($game->getPlayers()->count() >= 2)
+			throw $this->createAccessDeniedException("The game with ID {$game->getUuid()} is already full, please join another one.");
+
+		// If we can still join the game, join it as red player.
+		$onlineGame->joinAsPlayer($game, GamePlayer::Red);
+
+		return $this->redirectToRoute("onlineGame", [
+			"gameId" => $game->getUuid(),
+		]);
 	}
 
 	/**
@@ -68,6 +95,27 @@ class GameController extends AbstractController
 		// Try to find the game from its UUID.
 		if (empty($game = $onlineGame->findGame($gameId)))
 			throw $this->createNotFoundException("No game for ID $gameId");
+
+		if ($game->getPlayers()->count() < 2)
+		{ // The game doesn't have 2 players, wait for the other end to join or show them a form to join.
+			if (!empty($onlineGame->getPlayerUuid($game)))
+			{
+				// Initialize a response with autorefresh every 10 seconds.
+				$response = new Response();
+				$response->headers->set("Refresh", "10");
+
+				// Render the waiting page.
+				return $this->render("game/waiting.html.twig", [
+					"gameUrl" => $this->generateUrl("onlineGame", [ "gameId" => $game->getUuid() ], UrlGeneratorInterface::ABSOLUTE_URL),
+				], $response);
+			}
+			else
+			{ // Ask for a name to join the game.
+				return $this->render("game/join.html.twig", [
+					"gameId" => $game->getUuid(),
+				]);
+			}
+		}
 
 		// Return the rendered game.
 		return $this->render("game/index.html.twig", [
--- web/src/Game/OnlineGame.php
+++ web/src/Game/OnlineGame.php
@@ -3,7 +3,11 @@
 namespace App\Game;
 
 use App\Entity\Game;
+use App\Entity\GamePlayer;
+use App\Entity\OnlinePlayer;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Online game service.
@@ -11,10 +15,25 @@ use Doctrine\ORM\EntityManagerInterface;
 class OnlineGame
 {
 	/**
+	 * The online games cookie.
+	 */
+	const string COOKIE_NAME = "online-games";
+
+	/**
+	 * The updated online games attribute name in session.
+	 */
+	const string UPDATED_ONLINE_GAMES_ATTRIBUTE_NAME = "updatedOnlineGames";
+
+	/**
+	 * @param RequestStack $requestStack The request stack service.
 	 * @param GameState $gameState Game state service.
 	 * @param EntityManagerInterface $entityManager Entity manager service.
 	 */
-	public function __construct(private GameState $gameState, private EntityManagerInterface $entityManager)
+	public function __construct(
+		private RequestStack $requestStack,
+		private GameState $gameState,
+		private EntityManagerInterface $entityManager,
+	)
 	{
 	}
 
@@ -29,7 +48,28 @@ class OnlineGame
 		$this->entityManager->persist($game);
 		$this->entityManager->flush();
 
+		// Join the game as the green player.
+		$this->joinAsPlayer($game, GamePlayer::Green);
+
 		return $game;
+	}
+
+	/**
+	 * Create an online player for the provided game.
+	 * @param Game $game The online game.
+	 * @param GamePlayer $gamePlayer The game player ID of the new player.
+	 * @return OnlinePlayer The created online player.
+	 */
+	public function newOnlinePlayer(Game $game, GamePlayer $gamePlayer): OnlinePlayer
+	{
+		$onlinePlayer = new OnlinePlayer();
+		$onlinePlayer->setGame($game);
+		$onlinePlayer->setGamePlayer($gamePlayer);
+
+		$this->entityManager->persist($onlinePlayer);
+		$this->entityManager->flush();
+
+		return $onlinePlayer;
 	}
 
 	/**
@@ -40,5 +80,86 @@ class OnlineGame
 	public function findGame(string $uuid): Game|null
 	{
 		return $this->entityManager->getRepository(Game::class)->find($uuid);
+	}
+
+	/**
+	 * Join a game as the provided player.
+	 * @param Game $game The game to join.
+	 * @param GamePlayer $player The player ID (color) to be.
+	 * @return void
+	 */
+	public function joinAsPlayer(Game $game, GamePlayer $player): void
+	{
+		$this->registerOnlinePlayer(
+			$this->newOnlinePlayer($game, $player)
+		);
+	}
+
+	/**
+	 * Register the provided online player in the online games cookie.
+	 * @param OnlinePlayer $onlinePlayer The online player to register.
+	 * @return void
+	 */
+	public function registerOnlinePlayer(OnlinePlayer $onlinePlayer): void
+	{
+		$this->requestStack->getCurrentRequest()->getSession()->set(self::UPDATED_ONLINE_GAMES_ATTRIBUTE_NAME, [
+			$onlinePlayer->getGame()->getUuid() => $onlinePlayer->getUuid()
+		]);
+	}
+
+	/**
+	 * Get the player UUID of the current player (using online games cookie).
+	 * @param Game $game The game instance.
+	 * @return string|null The player UUID in the game, NULL if not a player of the game.
+	 */
+	public function getPlayerUuid(Game $game): string|null
+	{
+		return $this->getOnlineGamesCookie()[$game->getUuid()] ?? null;
+	}
+
+	/**
+	 * Get the online games cookie.
+	 * @return array<string, string> Game UUID => Player UUID associative array.
+	 */
+	private function getOnlineGamesCookie(): array
+	{
+		return json_decode($this->requestStack->getCurrentRequest()->cookies->get(self::COOKIE_NAME, "[]"), true);
+	}
+
+	/**
+	 * Create the cookie to store the online games in which the current player is.
+	 * @return Cookie The cookie to send back in response.
+	 */
+	public function createOnlineGamesCookie(): Cookie
+	{
+		// Create an online games cookie with online games update.
+		$cookie = Cookie::create(self::COOKIE_NAME)
+			->withValue(json_encode([
+				...$this->getOnlineGamesCookie(),
+				...$this->getUpdatedOnlineGames(),
+			]))
+			->withSecure();
+
+		$this->clearUpdatedOnlineGames();
+
+		return $cookie;
+	}
+
+	/**
+	 * Get the updated game online games.
+	 * @return array<string, string> Game UUID => Player UUID associative array.
+	 */
+	public function getUpdatedOnlineGames(): array
+	{
+		return $this->requestStack->getCurrentRequest()->getSession()->get(self::UPDATED_ONLINE_GAMES_ATTRIBUTE_NAME, []);
+	}
+
+	/**
+	 * Clear the updated online games.
+	 * @return void
+	 */
+	public function clearUpdatedOnlineGames(): void
+	{
+		$this->requestStack->getCurrentRequest()->getSession()->remove(self::UPDATED_ONLINE_GAMES_ATTRIBUTE_NAME);
 	}
 }
