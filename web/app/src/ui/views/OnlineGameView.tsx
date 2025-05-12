12111b622e78696404cb346fb7af03cdefc03604
Matthieu Hochlander <matthieu@marmelab.com>
2025-05-12 14:33:54 +0200

    Real-time online game view update.

--- web/app/src/ui/views/OnlineGameView.tsx
+++ web/app/src/ui/views/OnlineGameView.tsx
@@ -1,32 +1,73 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./GameView.css";
 import { GameBoard } from "../board/GameBoard";
 import { useFetchGame } from "../../api/games";
 import { PlayerTurn } from "../board/PlayerTurn";
-import { getGameGreenPlayer, getGameRedPlayer } from "../../model/game";
+import {
+	Game,
+	getGameGreenPlayer,
+	getGameRedPlayer,
+	zGame,
+} from "../../model/game";
+
+/**
+ * In milliseconds.
+ */
+const EVENT_SOURCE_FAILURE_RETRY_TIME = 1000;
+
+function startEventSource(
+	gameUuid: string,
+	setEventSource: (eventSource: EventSource) => void,
+	onGameState: (game: Game) => void,
+) {
+	const eventSource = new EventSource(`/.well-known/mercure?topic=${gameUuid}`);
+
+	eventSource.addEventListener("message", (event) => {
+		onGameState(zGame.parse(JSON.parse(event.data)));
+	});
+	eventSource.addEventListener("error", () => {
+		window.setTimeout(
+			() => startEventSource(gameUuid, setEventSource, onGameState),
+			EVENT_SOURCE_FAILURE_RETRY_TIME,
+		);
+	});
+
+	setEventSource(eventSource);
+}
 
 export function OnlineGameView() {
 	const gameUuid = useParams().uuid;
 	const fetchedGame = useFetchGame(gameUuid);
 
+	const eventSourceRef = useRef<EventSource | null>(null);
+	const [updatedGame, setUpdatedGame] = useState<Game | null>(null);
+
 	useEffect(() => {
-		// Refetch the game every 5 seconds.
-		const interval = setInterval(() => fetchedGame.refetch(), 5000);
-		return () => clearInterval(interval);
+		startEventSource(
+			gameUuid,
+			(eventSource) => (eventSourceRef.current = eventSource),
+			setUpdatedGame,
+		);
+
+		return () => {
+			eventSourceRef.current.close();
+		};
 	}, []);
+
+	const game = updatedGame ?? fetchedGame?.data;
 
 	return (
 		<>
 			<header>
 				<h1>
-					{getGameGreenPlayer(fetchedGame.data)?.name ?? "Green"} VS{" "}
-					{getGameRedPlayer(fetchedGame.data)?.name ?? "Red"}
+					{getGameGreenPlayer(game)?.name ?? "Green"} VS{" "}
+					{getGameRedPlayer(game)?.name ?? "Red"}
 				</h1>
 			</header>
 			<main className="game">
-				<GameBoard board={fetchedGame.data.board} />
-				<PlayerTurn game={fetchedGame.data} />
+				<GameBoard board={game.board} />
+				<PlayerTurn game={game} />
 			</main>
 		</>
 	);
