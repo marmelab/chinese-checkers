0aa1975a4828c15250322a55cd3ed04da8516907
Matthieu Hochlander <matthieu@marmelab.com>
2025-05-14 11:56:17 +0200

    Add confettis when winning an online game.

--- web/app/src/ui/views/OnlineGameView.tsx
+++ web/app/src/ui/views/OnlineGameView.tsx
@@ -10,12 +10,14 @@ import {
 	getCurrentPlayer,
 	getGameGreenPlayer,
 	getGameRedPlayer,
+	getWinnerPlayer,
 	isGameStarted,
 	zGame,
 } from "../../model/game";
 import { ErrorView } from "./ErrorView";
 import { getOnlineGamePlayerId } from "../../storage/online-game";
 import { PlayableGameBoard } from "../board/PlayableGameBoard";
+import { confetti } from "@tsparticles/confetti";
 
 export function OnlineGameView() {
 	const gameUuid = useParams().uuid;
@@ -39,6 +41,17 @@ export function OnlineGameView() {
 
 	const game = updatedGame ?? fetchedGame?.data;
 
+	const onlineGamePlayerId = getOnlineGamePlayerId(game?.uuid);
+
+	useEffect(() => {
+		if (game?.winner && getWinnerPlayer(game)?.uuid == onlineGamePlayerId)
+			confetti({
+				particleCount: 100,
+				spread: 70,
+				origin: { y: 0.6 },
+			});
+	}, [game, onlineGamePlayerId]);
+
 	if (!game) return <ErrorView />;
 
 	if (!isGameStarted(game))
@@ -58,8 +71,6 @@ export function OnlineGameView() {
 				</main>
 			</>
 		);
-
-	const onlineGamePlayerId = getOnlineGamePlayerId(game.uuid);
 
 	return (
 		<>
