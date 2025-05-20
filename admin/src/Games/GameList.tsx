aa1979ff1b5e642d758b05ed21cb189e4f696714
Matthieu Hochlander <matthieu@marmelab.com>
2025-05-20 17:53:35 +0200

    Add status computed field for games and show it.

--- admin/src/Games/GameList.tsx
+++ admin/src/Games/GameList.tsx
@@ -2,12 +2,28 @@ import {
 	ChipField,
 	Datagrid,
 	DateField,
+	FunctionField,
 	List,
 	ReferenceField,
 	ReferenceManyField,
 	SingleFieldList,
 	TextField,
 } from "react-admin";
+import { Chip } from "@mui/material";
+
+export const statusLabel: Record<string, string> = {
+	started: "Started",
+	pending: "Pending",
+	finished: "Finished",
+};
+export const statusLabelColor: Record<
+	keyof typeof statusLabel,
+	"info" | "primary" | "success" | undefined
+> = {
+	pending: undefined,
+	started: "primary",
+	finished: "success",
+};
 
 export function GameList() {
 	return (
@@ -17,11 +33,27 @@ export function GameList() {
 				field: "updated_at",
 				order: "DESC",
 			}}
+			queryOptions={{
+				meta: {
+					columns: ["*", "status"],
+				},
+			}}
 		>
 			<Datagrid>
 				<TextField source="uuid" />
-				<DateField source="created_at" />
-				<DateField source="updated_at" />
+				<DateField source="created_at" showTime />
+				<DateField source="updated_at" showTime />
+
+				<FunctionField
+					label={"Status"}
+					render={(record) => (
+						<Chip
+							label={statusLabel?.[record.status] ?? "Unknown"}
+							color={statusLabelColor?.[record.status] ?? undefined}
+						/>
+					)}
+				/>
+
 				<ReferenceManyField
 					label="Green"
 					reference="online_player"
@@ -40,6 +72,7 @@ export function GameList() {
 						</ReferenceField>
 					</SingleFieldList>
 				</ReferenceManyField>
+
 				<ReferenceManyField
 					label="Red"
 					reference="online_player"
