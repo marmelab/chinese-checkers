2f3dc97326975332cda3e4c1a5c2fcf375af7e30
Matthieu Hochlander <matthieu@marmelab.com>
2025-06-02 11:38:15 +0200

    Add a function to find valid moves from a provided cell, and handle jump moves in hint function.

--- internal/game/game_moves_test.go
+++ internal/game/game_moves_test.go
@@ -10,7 +10,7 @@ func TestFindBestMove(t *testing.T) {
 		board := NewDefaultBoard7()
 
 		bestMove := board.FindBestMove(Green)
-		assert.Equal(t, []CellIdentifier{{0, 3}, {0, 4}}, bestMove)
+		assert.Equal(t, []CellIdentifier{{0, 2}, {0, 4}}, bestMove)
 	}
 
 	{
@@ -34,4 +34,147 @@ func TestFindBestMove(t *testing.T) {
 			assert.Equal(t, []CellIdentifier{{0, 4}, {0, 3}}, bestMove)
 		}
 	}
+
+	{
+		board := NewDefaultBoard7()
+		board.Board = [][]Cell{
+			{1, 1, 1, 1, 0, 0, 0},
+			{1, 1, 0, 1, 0, 0, 0},
+			{1, 0, 0, 0, 0, 0, 0},
+			{1, 0, 0, 2, 0, 0, 2},
+			{0, 0, 1, 0, 2, 0, 2},
+			{0, 0, 0, 0, 2, 2, 2},
+			{0, 0, 0, 0, 2, 2, 2},
+		}
+
+		{
+			bestMove := board.FindBestMove(Green)
+			assert.Equal(t, []CellIdentifier{{0, 3}, {2, 3}, {4, 3}, {4, 5}}, bestMove)
+		}
+		{
+			bestMove := board.FindBestMove(Red)
+			assert.Equal(t, []CellIdentifier{{6, 5}, {4, 5}, {4, 3}, {4, 1}}, bestMove)
+		}
+	}
+}
+
+func TestFindPathsSimpleMove(t *testing.T) {
+	board := NewDefaultBoard7()
+	board.Board = [][]Cell{
+		{1, 1, 1, 1, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0, 0},
+		{1, 1, 0, 1, 0, 0, 0},
+		{1, 0, 0, 0, 0, 0, 2},
+		{0, 0, 0, 0, 0, 2, 2},
+		{0, 0, 0, 0, 2, 2, 2},
+		{0, 0, 0, 2, 2, 2, 2},
+	}
+	board.CurrentPlayer = Green
+
+	paths := board.FindValidMovesFrom(CellIdentifier{2, 3}, []CellIdentifier{}, true)
+
+	assert.Equal(t, PathsTree{
+		Cell: CellIdentifier{2, 3},
+		Move: []CellIdentifier{CellIdentifier{2, 3}},
+		Paths: []PathsTree{
+			{
+				Cell:  CellIdentifier{2, 4},
+				Move:  []CellIdentifier{CellIdentifier{2, 3}, CellIdentifier{2, 4}},
+				Paths: []PathsTree{},
+			},
+			{
+				Cell:  CellIdentifier{2, 2},
+				Move:  []CellIdentifier{CellIdentifier{2, 3}, CellIdentifier{2, 2}},
+				Paths: []PathsTree{},
+			},
+			{
+				Cell:  CellIdentifier{3, 3},
+				Move:  []CellIdentifier{CellIdentifier{2, 3}, CellIdentifier{3, 3}},
+				Paths: []PathsTree{},
+			},
+			{
+				Cell:  CellIdentifier{1, 3},
+				Move:  []CellIdentifier{CellIdentifier{2, 3}, CellIdentifier{1, 3}},
+				Paths: []PathsTree{},
+			},
+		},
+	}, paths, "should have 4 simple moves allowed")
+}
+
+func TestFindPathsSimpleJump(t *testing.T) {
+	board := NewDefaultBoard7()
+	board.Board = [][]Cell{
+		{1, 1, 1, 0, 0, 0, 0},
+		{1, 1, 0, 1, 0, 0, 0},
+		{0, 1, 1, 1, 0, 0, 0},
+		{1, 0, 0, 2, 0, 0, 2},
+		{0, 0, 0, 2, 0, 2, 2},
+		{0, 0, 0, 0, 0, 2, 2},
+		{0, 0, 0, 2, 0, 2, 2},
+	}
+	board.CurrentPlayer = Green
+
+	paths := board.FindValidMovesFrom(CellIdentifier{2, 3}, []CellIdentifier{}, true)
+
+	assert.Equal(t, PathsTree{
+		Cell: CellIdentifier{2, 3},
+		Move: []CellIdentifier{CellIdentifier{2, 3}},
+		Paths: []PathsTree{
+			{
+				Cell:  CellIdentifier{2, 4},
+				Move:  []CellIdentifier{CellIdentifier{2, 3}, CellIdentifier{2, 4}},
+				Paths: []PathsTree{},
+			},
+			{
+				Cell:  CellIdentifier{0, 3},
+				Move:  []CellIdentifier{CellIdentifier{2, 3}, CellIdentifier{0, 3}},
+				Paths: []PathsTree{},
+			},
+		},
+	}, paths, "should have one simple move and one jump allowed")
+}
+
+func TestFindPathsChainedJump(t *testing.T) {
+	board := NewDefaultBoard7()
+	board.Board = [][]Cell{
+		{1, 1, 0, 0, 1, 0, 0},
+		{1, 1, 0, 1, 0, 2, 0},
+		{0, 1, 1, 1, 0, 0, 0},
+		{1, 0, 0, 2, 0, 0, 0},
+		{0, 0, 0, 2, 0, 2, 2},
+		{0, 0, 0, 0, 0, 2, 2},
+		{0, 0, 0, 2, 0, 2, 2},
+	}
+	board.CurrentPlayer = Green
+
+	paths := board.FindValidMovesFrom(CellIdentifier{2, 3}, []CellIdentifier{}, true)
+
+	assert.Equal(t, PathsTree{
+		Cell: CellIdentifier{2, 3},
+		Move: []CellIdentifier{CellIdentifier{2, 3}},
+		Paths: []PathsTree{
+			{
+				Cell:  CellIdentifier{2, 4},
+				Move:  []CellIdentifier{CellIdentifier{2, 3}, CellIdentifier{2, 4}},
+				Paths: []PathsTree{},
+			},
+			{
+				Cell: CellIdentifier{0, 3},
+				Move: []CellIdentifier{CellIdentifier{2, 3}, CellIdentifier{0, 3}},
+				Paths: []PathsTree{
+					{
+						Cell: CellIdentifier{0, 5},
+						Move: []CellIdentifier{CellIdentifier{2, 3}, CellIdentifier{0, 3}, CellIdentifier{0, 5}},
+						Paths: []PathsTree{
+							{
+								Cell:  CellIdentifier{2, 5},
+								Move:  []CellIdentifier{CellIdentifier{2, 3}, CellIdentifier{0, 3}, CellIdentifier{0, 5}, CellIdentifier{2, 5}},
+								Paths: []PathsTree{},
+							},
+						},
+					},
+				},
+			},
+		},
+	}, paths, "should have one simple move and chained jumps to c6")
 }
