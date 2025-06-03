871242ab38aa8bf99214c941213c53ef0b926a25
Matthieu Hochlander <matthieu@marmelab.com>
2025-06-03 16:46:38 +0200

    Fix target cell.

--- internal/game/game_eval.go
+++ internal/game/game_eval.go
@@ -18,46 +18,14 @@ type DistanceEvaluation = struct {
 	Red   float64 `json:"red"`
 }
 
-// FindTargetCell for the provided player, used in distance evaluation.
-func (board *BoardState) FindTargetCell(player Player) CellIdentifier {
-	// Take the other corner of the board when for green target.
-	cornerOrigin := 0
-	cornerFactor := 1
-	if player == Green {
-		cornerOrigin = len(board.Board) - 1
-		cornerFactor = -1
-	}
-
-	// First check the edge of the target area.
-	for i := range 4 {
-		if board.Board[cornerOrigin+0][cornerOrigin+cornerFactor*i] != Cell(player) {
-			return CellIdentifier{Row: int8(cornerOrigin + 0), Column: int8(cornerOrigin + cornerFactor*i)}
-		}
-		if board.Board[cornerOrigin+cornerFactor*i][cornerOrigin+0] != Cell(player) {
-			return CellIdentifier{Row: int8(cornerOrigin + cornerFactor*i), Column: int8(cornerOrigin + 0)}
-		}
-	}
-	// Then check the 3 other cells of the target area.
-	for i := range 2 {
-		if board.Board[cornerOrigin+cornerFactor*1][cornerOrigin+cornerFactor*(i+1)] != Cell(player) {
-			return CellIdentifier{Row: int8(cornerOrigin + cornerFactor*1), Column: int8(cornerOrigin + cornerFactor*(i+1))}
-		}
-		if board.Board[cornerOrigin+cornerFactor*(i+1)][cornerOrigin+cornerFactor*1] != Cell(player) {
-			return CellIdentifier{Row: int8(cornerOrigin + cornerFactor*(i+1)), Column: int8(cornerOrigin + cornerFactor*1)}
-		}
-	}
-
-	return CellIdentifier{int8(cornerOrigin), int8(cornerOrigin)}
-}
-
 // Evaluate the distance score of each player.
 // The lower the better: a low score indicates that the player is near their target area.
 func (board *BoardState) EvaluateDistance() (result DistanceEvaluation) {
 	result.Green = 0
 	result.Red = 0
 
-	greenTarget := board.FindTargetCell(Green)
-	redTarget := board.FindTargetCell(Red)
+	greenTarget := CellIdentifier{6, 6}
+	redTarget := CellIdentifier{0, 0}
 
 	for rowIndex, row := range board.Board {
 		for columnIndex, cell := range row {
